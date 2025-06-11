0c50a94e85cf96c948b095f16b0aa266e1c77f21
Berkan <148571945+ozmberkan@users.noreply.github.com>
2025-06-11 11:54:50 +0300

    feat: Logo bileşeninde stil güncellemeleri yapıldı. Arka plan gradyanı eklendi.

--- client/src/components/UI/Logo/Logo.jsx
+++ client/src/components/UI/Logo/Logo.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/" className="font-bold tracking-tighter text-3xl text-text">
+    <Link
+      to="/"
+      className="font-bold tracking-tighter text-3xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent"
+    >
       Stoxon
     </Link>
   );
